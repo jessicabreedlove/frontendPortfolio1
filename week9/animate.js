e3a48911bc07f91516a586c8c8ea4a87f662d217
jessicabreedlove <jessicabreedlove@gmail.com>
2022-06-16 19:20:31 -0600

    animate example

---
+++ week9/animate.js
@@ -0,0 +1,19 @@
+const squareElement = document.getElementById('square');
+let angle = 0;
+
+function rotate() {
+  angle = (angle + 2) % 360;
+  squareElement.style.transform = `rotate(${angle}deg)`;
+  window.requestAnimationFrame(rotate);
+}
+
+const id = requestAnimationFrame(rotate);
+
+// The old way of doing it
+// const squareElement = document.getElementById('square');
+// let angle = 0;
+
+// setInterval(() => {
+//   angle = (angle + 2) % 360;
+//   squareElement.style.transform = `rotate(${angle}deg)`;
+// }, 1000 / 60);
